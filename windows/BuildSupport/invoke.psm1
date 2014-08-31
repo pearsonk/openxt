50bbf75f703e01abea0ed2a15c6315bf9f65617b
Kevin Pearson <pearson.kevin.m@gmail.com>
2014-08-31 18:51:56 -0400

    Added method to quote command arguments
    Prior to calling the command passed to Invoke-CommandChecked, quote the extracted parameter/value list to ensure whitespace is handled.
    This makes a few dangerous assumptions about the parameter/value list passed in keeping with conventions for Windows commands, scripts, and binaries:
    1) Parameter names begin with either a dash (-) or forward slash (/)
    2) Values do not and cannot begin with a dash or forward slash
    3) Powershell will not be invoked with anonymous parameters

--- windows/BuildSupport/invoke.psm1
+++ windows/BuildSupport/invoke.psm1
@@ -1,9 +1,15 @@
-# TODO: this is pasted from xc-windows.git/do_build.ps1 but I would like to find a way
-# to share code. For now if you fix a bug here please fix it in that version
-# as well.
-
-# run a command with output redirection so that we wait
-# and check the error code and fail if non-zero
+# TODO: this is pasted from xc-windows.git/do_build.ps1 but I would like to find a way
+
+# to share code. For now if you fix a bug here please fix it in that version
+
+# as well.
+
+
+
+# run a command with output redirection so that we wait
+
+# and check the error code and fail if non-zero
+
 function Invoke-CommandChecked {
   Write-Host ("Invoke "+([string]$args))
   $description = $args[0]
@@ -17,6 +23,8 @@ function Invoke-CommandChecked {
   $arglist.AddRange($args)
   # ... then remove the first two argument
   $arglist.RemoveRange(0,2)
+  # Quote the command arguments if not done so by invoker
+  Quote-CommandArguments -arguments $arglist
   Write-Host ("+$command "+[string]$arglist)
   & $command $arglist | Out-Host
   Write-Host "$command exited with code $LastExitCode"
@@ -25,3 +33,62 @@ function Invoke-CommandChecked {
   }
 }
 
+# Function to quote a command, passed as the parameter "arguments", which is of type 
+# System.Collections.ArrayList containing String values
+#
+# Note: This does not automatically fix malformatted commands which have 
+# unterminated quotes already. This also will cause problems for any unconventional
+# commands, scripts, or binaries
+function Quote-CommandArguments {
+  #Use named command parameter
+  Param([System.Collections.ArrayList]${arguments})
+  
+  ${original} = ${arguments}.Clone()
+  # Get array of indices for parameters (valued beginning with '-')
+  ${parameters} = New-Object System.Collections.ArrayList
+  for (${index}=0;${index} -lt ${arguments}.Count; ${index}++)
+  {
+    # Check if current value is a parameter declaration
+    # Dash (-) for powershell and some other command types
+    # Forward slash (/) for common Windows applications
+    if (${arguments}[${index}] -match "^[-/]")
+    {
+        ${parameters}.add(${index}) | out-null
+    }
+  }
+  
+  # Iterate the list of parameters to ensure they are quoted
+  for (${index}=0;${index} -lt ${parameters}.Count; ${index}++)
+  {
+    # Skip parameters with no value
+    ## Left half checks if last parameter is at end of command string
+    ## Right half checks if another parameter immediately follows the current parameter
+    if (${parameters}[${index}] -eq ${arguments}.Count - 1 -or $parameters[$index] + 1 -eq $parameters[$index + 1])
+    {
+      # 
+      continue
+    }
+    
+    # Handle where we are at the end of the array.
+    ${end} = ${parameters}[${index} + 1] - 1
+    if (${index} -eq ${parameters}.Count - 1)
+    {
+      ${end} = ${arguments}.Count - 1
+    }
+    
+    # If not already quoted do so
+    ## Regex matches beginning of String with single or double quote, double quote escaped with backtick
+    ## Note, this does not handle 
+    if (! (${arguments}[${parameters}[${index}] + 1] -match "^['`"]"))
+    {
+        ## Quote the 
+        ${arguments}[${parameters}[${index}] + 1] = "'"+${arguments}[${parameters}[${index}] + 1]
+        ${arguments}[${end}] = ${arguments}[${end}]+"'"
+    }
+  }
+  
+  Write-Host ${MyInvocation}.MyCommand ": Changed arguments from"
+  Write-Host ${MyInvocation}.MyCommand ": `t" ${original}
+  Write-Host ${MyInvocation}.MyCommand ": To"
+  Write-Host ${MyInvocation}.MyCommand ":`t" ${arguments}
+}
